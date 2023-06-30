550fe735b302f4f7960b2aaf7ef7f026fa2675cc
Marco Mistroni <mmistroni@gmail.com>
2023-06-30 06:52:05 +0100

    adding tracer

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -4,11 +4,69 @@ import random
 import redis
 import socket
 import sys
+import logging
+from datetime import datetime
+# App Insights
+# TODO: Import required libraries for App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+# For metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
+# Adding logger for custom events
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
+# Standard Logging
+logger = logging.getLogger(__name__)
+handler = AzureLogHandler(connection_string='InstrumentationKey=9559e5cb-afcb-4a47-8b78-79c42de51f91')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+# Logging custom Events 
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=9559e5cb-afcb-4a47-8b78-79c42de51f91'))
+# Set the logging level
+logger.setLevel(logging.INFO)
+
+## Adding metrics
+# Metrics
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=9559e5cb-afcb-4a47-8b78-79c42de51f91')
+view_manager.register_exporter(exporter)
+
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=9559e5cb-afcb-4a47-8b78-79c42de51f91'),
+    sampler=ProbabilitySampler(1.0),
+)
+
 
 app = Flask(__name__)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
+
+
+# Requests
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=9559e5cb-afcb-4a47-8b78-79c42de51f91"),
+    sampler=ProbabilitySampler(rate=1.0)
+)
+
+
+
 
 if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
     button1 = os.environ['VOTE1VALUE']
@@ -47,6 +105,10 @@ except redis.ConnectionError:
     exit('Failed to connect to Redis, terminating.')
 """
 
+
+
+
+
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
@@ -62,6 +124,16 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
+	# TODO: use tracer object to trace cat vote
+        with tracer.span(name="Cats Vote") as span:
+            print("Cats Vote")
+
+	# TODO: use tracer object to trace dog vote
+        with tracer.span(name="Dogs Vote") as span:
+            print("Dogs Vote")		
+
+
+
         vote2 = r.get(button2).decode('utf-8')
 
         # Return index with values
@@ -92,7 +164,13 @@ def index():
             
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')  
+	    properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            # TODO: use logger object to log cat vote
+            logger.info('Cats Vote', extra=properties)	
+
+            vote2 = r.get(button2).decode('utf-8') 
+            # TODO: use logger object to log dog vote
+            logger.info('Dogs Vote', extra=properties)   
                 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
